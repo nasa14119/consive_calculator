6ff9309ed995e9c80708cf22118bb2930080f7d5
nicolas amaya <nicolasamaya@MacBook-Pro-de-nicolas.local>
2024-06-26 13:15:45 -0600

    fist-commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import moment from 'moment/min/moment-with-locales'
+import './index.css'
+moment.locale("es")
+
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl text-center font-bold'>Conceibe Calculator</h1>
+    </>
+  )
+}
+
+export default App
